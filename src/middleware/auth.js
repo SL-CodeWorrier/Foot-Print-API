abd7b527e45687874d58912d4ac1d2f6e3cb067f
SL-CodeWorrier <chathuathnayaka@gmail.com>
2025-04-23 23:13:28 +0530

    changed auth again

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,11 +5,13 @@ const auth = async (req, res, next) => {
   try {
     const token = req.header('Authorization').replace('Bearer ', '');
     const decoded = jwt.verify(token, 'twittercourse');
-    //const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
 
-    //if (!user) {
-    //  throw new Error('Authentication failed!');
-    //}
+    // Only validate using _id
+    const user = await User.findOne({ _id: decoded._id });
+
+    if (!user) {
+      throw new Error('Authentication failed!');
+    }
 
     req.user = user;
     req.token = token;
