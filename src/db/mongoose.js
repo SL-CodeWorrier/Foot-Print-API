59ee7632ab4809f890824b3ccdebc4f553507edf
SL-CodeWorrier <chathuathnayaka@gmail.com>
2025-04-23 12:16:48 +0530

    modify the connection of mongodb

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const uri = "mongodb+srv://chathuathnayaka:Jq5IRuUuw81YQUDn@cluster0.ufru5zk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+//const uri = "mongodb+srv://chathuathnayaka:Jq5IRuUuw81YQUDn@cluster0.ufru5zk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://chathuathnayaka:Jq5IRuUuw81YQUDn@cluster0.ufru5zk.mongodb.net/footprintdb?retryWrites=true&w=majority&appName=Cluster0";
 
 mongoose.connect(uri, {
   useNewUrlParser: true,
